77c09b2fd61fee32118f972282638cd156bcffc0
João Campos <joaon.c.jv@gmail.com>
2024-01-09 11:58:08 -0300

    feito o layout inicial

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contacts from './components/pages/Contacts';
 import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container'
+
 
 function App() {
   return (
@@ -10,15 +12,17 @@ function App() {
       <ul>
         <Link to="/">Home</Link>
         <Link to="/contacts">Contato</Link>
-        <Link to="/company">Company</Link>
+        <Link to="/company">Empresa</Link>
         <Link to="/newproject">Novo Projeto</Link>
       </ul>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/newproject" element={<NewProject />} />
-      </Routes>
+      <Container customClass='min-height'>
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
       <p>Footer</p>
     </Router>
   );
