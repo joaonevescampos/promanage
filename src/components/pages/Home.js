933658a58820b2415725ae47ed5c1796bcc2e410
João Campos <joaon.c.jv@gmail.com>
2024-01-13 20:16:00 -0300

    criação da home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,14 @@
 import styles from './Home.module.css'
 import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 
 const Home = () => {
     return (
         <section className={styles.home_container}>
             <h1>Bem vindo ao <span>Pro Manage</span></h1>
             <p>Gerencie seus projetos agora mesmo!</p>
-            <a href="/">Criar Projeto</a>
-           
+            <LinkButton to='/newproject' text='Criar Projeto'/>
             <img src={savings} alt='Pro Manage'/>
         </section>
     )
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,12 @@
-const NewProject= () => {
+import styles from './NewProject.module.css'
+
+const NewProject = () => {
     return (
-        <h1>New Project</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para adicionar os serviços em seguida.</p>
+            <p>Formulário</p>
+        </div>
     )
 }
 
