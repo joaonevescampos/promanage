6d3241ad7b78945b04f9a2ab57acb0faf8bb98e8
João Campos <joaon.c.jv@gmail.com>
2024-01-13 15:18:45 -0300

    consertado os conflitos

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,32 +1,17 @@
-<<<<<<< HEAD
 import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa'
 import styles from './Footer.module.css'
 
 const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <ul className={styles.social_list}>
-                <li><FaFacebook /></li>
-                <li><FaInstagram /></li>
-                <li><FaLinkedin/></li>
-            </ul>
-            <p><span className={styles.copy_right}>Pro Manage</span> &copy; Todos Diretos Reservados - 2024</p>
-        </footer>
-=======
-import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
-// import styles from './Footer.module.css'
-
-const Footer = () => {
-    return (
-      <Footer>
-        <ul>
-          <li><FaFacebook /></li>
-          <li><FaInstagram /></li>
-          <li><FaLinkedin /></li>
-        </ul>
-      </Footer>
->>>>>>> 01ea8ba (atualização)
-    )
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li><FaFacebook /></li>
+        <li><FaInstagram /></li>
+        <li><FaLinkedin /></li>
+      </ul>
+      <p><span className={styles.copy_right}>Pro Manage</span> &copy; Todos Diretos Reservados - 2024</p>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
+
 import { Link } from 'react-router-dom'
 import Container from './Container'
 import styles from './Navbar.module.css'
 import logo from '../../img/costs_logo.png'
 
-=======
-import {Link} from 'react-router-dom'
-import Container from './Container'
-import styles from './Navbar.module.css'
-import logo from '../../img/costs_logo.png'
->>>>>>> 01ea8ba (atualização)
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <Container>
-<<<<<<< HEAD
                 <Link to="/">
                     <img src={logo} alt='promanage' />
                 </Link>
@@ -33,19 +26,6 @@ const Navbar = () => {
                     </li>
                 </ul>
             </Container>
-            
-=======
-                <Link to='/'>
-                    <img src={logo} alt="Pro Manage" />
-                </Link>
-                <ul className={styles.list}>
-                    <li className={styles.item}> <Link to="/">Home</Link></li>
-                    <li className={styles.item}> <Link to="/projects">Projetos</Link></li>
-                    <li className={styles.item}> <Link to="/company">Empresa</Link></li>
-                    <li className={styles.item}> <Link to="/contacts">Contato</Link></li>
-                </ul>
-            </Container>
->>>>>>> 01ea8ba (atualização)
         </nav>
     )
 }
