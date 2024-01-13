dc1776c14984cc128311a86d3f55841f04f7840d
João Campos <joaon.c.jv@gmail.com>
2024-01-13 11:08:04 -0300

    terminando css do navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
-import Contacts from './components/pages/Contacts';
+import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
 
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contacts">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </ul>
+      <Navbar />
       <Container customClass='min-height'>
         <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/company" element={<Company />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/newproject" element={<NewProject />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p><span className={styles.copy_right}>Pro Manage</span> &copy; Todos Diretos Reservados - 2024</p>
+        </footer>
+    )
+}
+
+export default Footer
