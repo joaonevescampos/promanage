d32d83cb7faa2ef131a62afb63d4b1060d6a1c7a
João Campos <joaon.c.jv@gmail.com>
2024-01-13 14:35:57 -0300

    feito o css do navbar e footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,14 @@
+// import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <section>
+            <h1>Bem vindo ao <span>Pro Manage</span></h1>
+            <p>Gerencie seus projetos agora mesmo!</p>
+            <a href="/">Criar Projeto</a>
+            <img src={savings} alt='Pro Manage'/>
+
+        </section>
     )
 }
 
