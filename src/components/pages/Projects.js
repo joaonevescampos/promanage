ca10f3f81b413f2c75f7d5607bb246648a8387b3
João Campos <joaon.c.jv@gmail.com>
2024-01-17 15:27:40 -0300

    criando loading

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,9 +5,12 @@ import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
 import { useState, useEffect } from 'react'
 import ProjectCard from '../projects/ProjectCard'
+import Loading from '../layout/Loading'
 
 const Projects = () => {
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+
     const location = useLocation()
     let message = ''
     if (location.state) {
@@ -15,19 +18,20 @@ const Projects = () => {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((resp) => resp.json())
-            .then((data) => { 
-                setProjects(data)
-                console.log(data)
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             })
-            .catch((err) => console.log(err))
-        
+                .then((resp) => resp.json())
+                .then((data) => { 
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 3000)
     }, [])
 
     return (
@@ -48,6 +52,10 @@ const Projects = () => {
                         />
                     ))
                 }
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
